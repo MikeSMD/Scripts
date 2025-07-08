e7bb66fc3ff971b4dece9e7a17f81c328d02b0b3
MikeSMD <m.schmidt.2k@gmail.com>
2025-07-08 12:22:47 +0200

    fuzzy algorithm finished with only few rules

--- fuzzy_battery_estimator/battery_estimator.py
+++ fuzzy_battery_estimator/battery_estimator.py
@@ -55,13 +55,13 @@ def get_temp():
 # -----------------------------------------
 
 def fuzzy_linear_decreasing(v: int, b: int, c: int) -> float:
-    if v >= c or v <= b :
+    if v > c or v < b :
         return 0.0
     else:
         return (c - v) / ( c - b )
 
 def fuzzy_linear_increasing(v: int, b: int, c: int) -> float:
-    if ( v <= b or v >= c ):
+    if ( v < b or v > c ):
         return 0.0
     else:
         return 1-(c - v) / ( c - b )
@@ -90,6 +90,7 @@ def fuzzify_temp(x):
     }
 
 def fuzzify_usage(x):
+    print ( x )
     return {
         "low": fuzzy_linear_decreasing(x, 0, 35),
         "medium": fuzzy_triangle(x,25, 45, 60),
@@ -102,16 +103,84 @@ def fuzzify_per(x):
         "medium": fuzzy_triangle(x,30, 45, 60),
         "high": fuzzy_triangle(x, 55, 70, 90),
         "max": fuzzy_linear_increasing(x, 80, 100),
-    }
+        }
+rules = [
+    {"inputs": {"jas": "max", "temp": "max", "usage": "max", "per": "low"}, "output": {"vydrz": "low"}, "operator": "AND"},
+    {"inputs": {"jas": "high", "temp": "high", "usage": "high", "per": "low"}, "output": {"vydrz": "low"}, "operator": "AND"},
+    {"inputs": {"jas": "high", "temp": "high", "usage": "high", "per": "medium"}, "output": {"vydrz": "low"}, "operator": "AND"},
+    {"inputs": {"jas": "medium", "temp": "medium", "usage": "medium", "per": "low"}, "output": {"vydrz": "low"}, "operator": "AND"},
+    {"inputs": {"jas": "medium", "temp": "medium", "usage": "medium", "per": "medium"}, "output": {"vydrz": "medium"}, "operator": "AND"},
+    {"inputs": {"jas": "low", "temp": "low", "usage": "low", "per": "medium"}, "output": {"vydrz": "high"}, "operator": "AND"},
+    {"inputs": {"jas": "low", "temp": "low", "usage": "low", "per": "high"}, "output": {"vydrz": "max"}, "operator": "AND"},
+    {"inputs": {"jas": "high", "temp": "medium", "usage": "low", "per": "medium"}, "output": {"vydrz": "medium"}, "operator": "AND"},
+    {"inputs": {"jas": "low", "temp": "medium", "usage": "high", "per": "medium"}, "output": {"vydrz": "medium"}, "operator": "AND"},
+    {"inputs": {"jas": "medium", "temp": "low", "usage": "medium", "per": "high"}, "output": {"vydrz": "high"}, "operator": "AND"},
+    {"inputs": {"jas": "high", "temp": "low", "usage": "low", "per": "max"}, "output": {"vydrz": "high"}, "operator": "AND"},
+    {"inputs": {"jas": "high", "temp": "medium", "usage": "medium", "per": "max"}, "output": {"vydrz": "medium"}, "operator": "AND"},
+    {"inputs": {"jas": "high", "temp": "medium", "usage": "medium", "per": "medium"}, "output": {"vydrz": "low"}, "operator": "AND"},
+    {"inputs": {"jas": "low", "temp": "medium", "usage": "low", "per": "medium"}, "output": {"vydrz": "medium"}, "operator": "AND"},
+{"inputs": {"jas": "medium", "temp": "high", "usage": "low", "per": "medium"}, "output": {"vydrz": "medium"}, "operator": "AND"},
+    {"inputs": {"jas": "low", "temp": "high", "usage": "medium", "per": "low"}, "output": {"vydrz": "medium"}, "operator": "AND"},
+    {"inputs": {"jas": "low", "temp": "high", "usage": "high", "per": "low"}, "output": {"vydrz": "low"}, "operator": "AND"},
+    {"inputs": {"jas": "max", "temp": "medium", "usage": "medium", "per": "medium"}, "output": {"vydrz": "low"}, "operator": "AND"},
+    {"inputs": {"jas": "medium", "temp": "low", "usage": "high", "per": "medium"}, "output": {"vydrz": "medium"}, "operator": "AND"},
+    {"inputs": {"jas": "low", "temp": "medium", "usage": "high", "per": "low"}, "output": {"vydrz": "medium"}, "operator": "AND"},
+    {"inputs": {"jas": "high", "temp": "max", "usage": "medium", "per": "medium"}, "output": {"vydrz": "low"}, "operator": "AND"},
+    {"inputs": {"jas": "max", "temp": "low", "usage": "low", "per": "high"}, "output": {"vydrz": "high"}, "operator": "AND"},]
+
+
+
+def firing_strength(brightness, temp, usage, per, rule):
+    firing_strength = min(
+        brightness[rule["inputs"]["jas"]],
+        temp[rule["inputs"]["temp"]],
+        usage[rule["inputs"]["usage"]],
+        per[rule["inputs"]["per"]]
+    )
+    return {rule["output"]["vydrz"]: firing_strength}
+
+
+def evaluate_rules(brightness, temp, usage, per, rules):
+    return [firing_strength(brightness, temp, usage, per, rule) for rule in rules]
+
+
+
+def aggregate_outputs(rule_outputs):
+    aggregated = {"low": 0.0, "medium": 0.0, "high": 0.0, "max": 0.0}
+    for output in rule_outputs:
+        for key, value in output.items():
+            aggregated[key] = max(aggregated[key], value)
+    return aggregated
+boundries = {
+    "low": 1,      # střed rozsahu (0+2)/2 = 1
+    "medium": 3.5, # střed rozsahu (2+5)/2 = 3.5
+    "high": 6.5,   # střed rozsahu (5+8)/2 = 6.5
+    "max": 9       # střed rozsahu (8+10)/2 = 9
+}
+
+def defuzzification(boundries, aggregated):
+    s = 0.0
+    p = 0.0
+    for k in ["low", "medium", "high", "max"]:
+        s += boundries[k] * aggregated[k]  # Reprezentativní hodnota * příslušnost
+        p += aggregated[k]
+    return s / p if p != 0 else 0.0
+
 
 def main():
+
+    if ( charging() ):
+        print("zarizeni se nabiji doba za jak dlouho se vybije je odhadnuta kdyby se nyni napajeni odpojilo")
+    
     brightness = fuzzify_jas( int ( get_jas() ) )
     temp = fuzzify_temp ( int ( get_temp() ) / 1000 )
     usage = fuzzify_usage ( int ( cpu_load() ) )
-    #charge = fuzzify_charging( charging() )
     per =fuzzify_per( int (get_per() ) )
     
-    
+    rule_outputs = evaluate_rules(brightness, temp, usage, per, rules)
+    aggregated = aggregate_outputs(rule_outputs)
+
+    s = defuzzification(boundries,aggregated)
 
 
 
@@ -119,6 +188,8 @@ def main():
     print(f"teplota jednotky: {temp}")
     print(f"usage {usage}")
     print(f"per {per}")
+    print( f"aggregated {aggregated}" )   # max ze vsech pravidel dle vystpup
+    print( f"odhadovana vydrz - {s}" )
 
 
 
